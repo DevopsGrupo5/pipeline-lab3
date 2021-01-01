3350bcf7f0e471e23f56f73b98844b94b0c9845f
Roddy Vitali <roddicks@gmail.com>
2020-12-31 22:32:59 -0300

    try catch utils

--- src/org/cl/Utils.groovy
+++ src/org/cl/Utils.groovy
@@ -1,13 +1,24 @@
 package org.cl
 
 def getData() {
-	def request = libraryResource 'org/cl/data.json'
-	return readJSON text: request
+	try{
+		def request = libraryResource 'org/cl/data.json'
+		return readJSON text: request
+	} catch (e){
+		println(e)
+		return readJSON text: "{}"
+	}
+
 }
 
 def cleanRepo(String url) {
-	println("cleanRepo $url")
-	return url.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')
+	try{
+		println("cleanRepo $url")
+		return url.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')
+	} catch (e) {
+		println(e)
+		return "ms-iclab"
+	}
 }
 
 return this;

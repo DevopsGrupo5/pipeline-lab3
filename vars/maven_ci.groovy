0744fdc6fccc291369d2495d06965af4c4b50c1d
Roddy Vitali <roddicks@gmail.com>
2021-01-04 20:47:11 -0300

    remove branch 0.0.2

--- vars/maven_ci.groovy
+++ vars/maven_ci.groovy
@@ -1,7 +1,6 @@
 import org.cl.*
 
 def call(flow) {
-    sh "git branch -d release-v0.0.2"
     sh "git checkout -b release-v0.0.2"
     withCredentials([usernamePassword(credentialsId: 'git-grupo5', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh "git push https://$USERNAME:$PASSWORD@github.com/DevopsGrupo5/ms-iclab-test.git release-v0.0.2"

a40627d048d562dc36b36c958819449126e6d114
Rafael Ortiz <rafael.ortiz.barrera@gmail.com>
2021-01-04 19:15:18 -0300

    cambio sintaxis clousure

--- vars/maven_cd.groovy
+++ vars/maven_cd.groovy
@@ -2,20 +2,20 @@ import org.cl.*
 
 def call(flow) {
     if (flow.canRunStage(StepEnum.GIT_DIFF)) {
-        stage(StepEnum.GIT_DIFF) {
+        stage(StepEnum.GIT_DIFF.getNombre()) {
             env.FAILED_STAGE = StepEnum.GIT_DIFF
             sh 'git diff origin/master'
         }
     }
     if (flow.canRunStage(StepEnum.NEXUS_DOWNLOAD)) {
-        stage(StepEnum.NEXUS_DOWNLOAD) {
+        stage(StepEnum.NEXUS_DOWNLOAD.getNombre()) {
 		    env.FAILED_STAGE = StepEnum.NEXUS_DOWNLOAD
             // sh 'curl -X GET -u admin:123456 http://1f84c9788a2a.ngrok.io/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O'
             // sh 'ls -ltr'
         }
     }
     if (flow.canRunStage(StepEnum.RUN)) {
-        stage(StepEnum.RUN) {
+        stage(StepEnum.RUN.getNombre()) {
 		    env.FAILED_STAGE = StepEnum.RUN
             withEnv(['JENKINS_NODE_COOKIE=dontkillme']) {
                 sh 'java -version'
@@ -26,7 +26,7 @@ def call(flow) {
         }
     }
     if (flow.canRunStage(StepEnum.TEST)) {
-        stage(StepEnum.TEST) {
+        stage(StepEnum.TEST.getNombre()) {
 		    env.FAILED_STAGE = StepEnum.TEST
             println 'Esperando a que inicie el servidor'
             sleep(time: 10, unit: "SECONDS")
@@ -39,7 +39,7 @@ def call(flow) {
         }
     }
     if (flow.canRunStage(StepEnum.GIT_MERGE_MASTER)) {
-        stage(StepEnum.GIT_MERGE_MASTER) {
+        stage(StepEnum.GIT_MERGE_MASTER.getNombre()) {
 		    env.FAILED_STAGE = StepEnum.GIT_MERGE_MASTER
             sh 'git pull origin master'
             sh 'git merge master'
@@ -48,7 +48,7 @@ def call(flow) {
         }
     }
     if (flow.canRunStage(StepEnum.GIT_MERGE_DEVELOP)) {
-        stage(StepEnum.GIT_MERGE_DEVELOP) {
+        stage(StepEnum.GIT_MERGE_DEVELOP.getNombre()) {
             env.FAILED_STAGE = StepEnum.GIT_MERGE_DEVELOP
             sh 'git pull origin develop'
             sh 'git merge develop'
@@ -57,7 +57,7 @@ def call(flow) {
         }
     }
     if (flow.canRunStage(StepEnum.GIT_TAG_MASTER)) {
-        stage(StepEnum.GIT_TAG_MASTER) {
+        stage(StepEnum.GIT_TAG_MASTER.getNombre()) {
             env.FAILED_STAGE = StepEnum.GIT_TAG_MASTER
             sh 'git push -f origin/master v1.0.0'
         }
--- vars/maven_ci.groovy
+++ vars/maven_ci.groovy
@@ -2,25 +2,25 @@ import org.cl.*
 
 def call(flow) {
     if (flow.canRunStage(StepEnum.COMPILE)) {
-        stage(StepEnum.COMPILE) {
+        stage(StepEnum.COMPILE.getNombre()) {
 		    env.FAILED_STAGE = StepEnum.COMPILE
             sh './mvnw clean compile -e'
         }
     }
     if (flow.canRunStage(StepEnum.UNIT_TEST)) {
-        stage(StepEnum.UNIT_TEST) {
+        stage(StepEnum.UNIT_TEST.getNombre()) {
 		    env.FAILED_STAGE = StepEnum.UNIT_TEST
             sh './mvnw clean test -e'
         }
     }
     if (flow.canRunStage(StepEnum.JAR)) {
-        stage(StepEnum.JAR) {
+        stage(StepEnum.JAR.getNombre()) {
 		    env.FAILED_STAGE = StepEnum.JAR
             sh './mvnw clean package -e'
         }
     }
     if (flow.canRunStage(StepEnum.SONAR)) {
-        stage(StepEnum.SONAR) {
+        stage(StepEnum.SONAR.getNombre()) {
             env.FAILED_STAGE = StepEnum.SONAR
             withSonarQubeEnv(installationName: 'sonar') {
                 sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
@@ -28,13 +28,13 @@ def call(flow) {
         }
     }
     if (flow.canRunStage(StepEnum.NEXUS_UPLOAD)) {
-        stage(StepEnum.NEXUS_UPLOAD) {
+        stage(StepEnum.NEXUS_UPLOAD.getNombre()) {
             env.FAILED_STAGE = StepEnum.NEXUS_UPLOAD
 	        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'grupo-5', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: '/var/lib/jenkins/workspace/ms-iclab-test/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
         }
     }
     if (flow.canRunStage(StepEnum.GIT_CREATE_RELEASE)) {
-        stage(StepEnum.GIT_CREATE_RELEASE) {
+        stage(StepEnum.GIT_CREATE_RELEASE.getNombre()) {
 		    env.FAILED_STAGE = StepEnum.GIT_CREATE_RELEASE
             sh "git branch -d release-v0.0.1"
             sh "git checkout -b release-v0.0.1"

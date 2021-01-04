90661556595c10db0558b1b0aa32b419fad56acb
Roddy Vitali <roddicks@gmail.com>
2021-01-04 00:51:27 -0300

    add sonar and nexus

--- vars/maven_ci.groovy
+++ vars/maven_ci.groovy
@@ -1,18 +1,6 @@
 import org.cl.*
 
 def call(flow) {
-    if (flow.canRunStage(Step.GIT_CREATE_RELEASE)) {
-        stage(Step.GIT_CREATE_RELEASE) {
-		    env.FAILED_STAGE = Step.GIT_CREATE_RELEASE
-            // sh "git remote set-url origin git@github.com:DevopsGrupo5/ms-iclab-test.git"
-            sh "git remote -v"
-            sh "git remote show origin"
-            sh "git branch -d release-v0.0.1"
-            sh "git checkout -b release-v0.0.1"
-            // sh 'git push origin origin/release-v0.0.1'
-            sh 'git push https://DiplomadoDevOps5:dev123ops@github.com/DevopsGrupo5/ms-iclab-test.git release-v0.0.1'
-        }
-    }
     if (flow.canRunStage(Step.COMPILE)) {
         stage(Step.COMPILE) {
 		    env.FAILED_STAGE = Step.COMPILE
@@ -34,18 +22,25 @@ def call(flow) {
     if (flow.canRunStage(Step.SONAR)) {
         stage(Step.SONAR) {
             env.FAILED_STAGE = Step.SONAR
-            // withSonarQubeEnv(installationName: 'lab_sonar') { // You can override the credential to be used
-            //     sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
-            // }
+            withSonarQubeEnv(installationName: 'sonar') {
+                sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+            }
         }
     }
     if (flow.canRunStage(Step.NEXUS_UPLOAD)) {
         stage(Step.NEXUS_UPLOAD) {
             env.FAILED_STAGE = Step.NEXUS_UPLOAD
-            // nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/selyt2020/Documents/GitHub/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+	        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'grupo-5', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: '/var/lib/jenkins/workspace/ms-iclab-test/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
         }
     }
-
+    if (flow.canRunStage(Step.GIT_CREATE_RELEASE)) {
+        stage(Step.GIT_CREATE_RELEASE) {
+		    env.FAILED_STAGE = Step.GIT_CREATE_RELEASE
+            sh "git branch -d release-v0.0.1"
+            sh "git checkout -b release-v0.0.1"
+            sh 'git push https://DiplomadoDevOps5:dev123ops@github.com/DevopsGrupo5/ms-iclab-test.git release-v0.0.1'
+        }
+    }
 }
 
 return this;

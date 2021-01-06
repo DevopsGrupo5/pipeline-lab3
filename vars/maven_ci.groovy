f2b6e66c061cf23a21c07f981c6f0edebd835692
sebassabes <53450861+sebassabes@users.noreply.github.com>
2021-01-06 09:44:49 -0300

    Update maven_ci.groovy
    Cambio al comando de "git branch -d" a "git branch -D" para asi poder eliminar a la fuerza la rama "release-v0.0.1"

--- vars/maven_ci.groovy
+++ vars/maven_ci.groovy
@@ -36,7 +36,7 @@ def call(flow) {
     if (flow.canRunStage(StepEnum.GIT_CREATE_RELEASE)) {
         stage(StepEnum.GIT_CREATE_RELEASE.getNombre()) {
 		    env.FAILED_STAGE = StepEnum.GIT_CREATE_RELEASE
-            sh "git branch -d release-v0.0.1"
+            sh "git branch -D release-v0.0.1"
             sh "git checkout -b release-v0.0.1"
             withCredentials([usernamePassword(credentialsId: 'git-crendentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
                 sh "git push https://$USERNAME:$PASSWORD@github.com/DevopsGrupo5/ms-iclab-test.git release-v0.0.1"

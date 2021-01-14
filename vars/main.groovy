7ccb30ba855dbda80fa2935c6028a331898547c9
Rafael Ortiz <rafael.ortiz.barrera@gmail.com>
2021-01-14 19:47:49 -0300

    solucion de case string

--- vars/main.groovy
+++ vars/main.groovy
@@ -49,6 +49,7 @@ def call() {
 
                         stage('Validation') {
                             println 'branch type ' + branchType
+                            println 'valid ' + isValidBranch()
                             if (!flow.isValidBranch()) {
                                 env.ERROR_MESSAGE = "Branch Type $branchType is not valid!"
                                 throw new Exception(env.ERROR_MESSAGE)
